afb0e426de86275032cca7ee7b27a6c49fef478c
Adnan Bhuiyan <adnanb59@users.noreply.github.com>
2020-06-19 17:01:09 -0400

    Add files via upload

---
+++ src/main/java/dev/adnanb/spring_maven_sql_demo/SpringMavenSqlDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.adnanb.spring_maven_sql_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMavenSqlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMavenSqlDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/adnanb/spring_maven_sql_demo/SpringMavenSqlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.adnanb.spring_maven_sql_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMavenSqlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
